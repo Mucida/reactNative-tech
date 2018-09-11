316db36e36e8cab79c0d8e2b3b58a27e7415507d
mucida <lucas.mucida@ufv.br>
2018-09-11 15:46:52 -0300

    aula 89 - 90 - 91 - 92

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+	renderItem(library) {
+
+	}
+
 	render() {
-		return;
+		return(
+			<FlatList 
+				data={this.props.librarie}
+				renderItem={this.renderItem}
+				keyExtractor={(library) => library.id}
+			/>
+		);
 	}
 }
 
-export default connect()(LibraryList);
+const mapStateToProps = state =>{
+	return { libraries: state.libraries }; //this becomes a prop to the component
+};
+
+export default connect(mapStateToProps)(LibraryList);
+//this line above (connect) becomes a link betwen libraryList and application state
+//connection between react side and redux side
